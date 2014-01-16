72f701eb1be448f89dd85110588c67c720805d87
Divya Nair <dsnair@users.noreply.github.com>
2014-01-16 15:52:46 -0700

    solve recurrence relation

--- UNR_Numerical_Analysis/c1s3p4.c
+++ UNR_Numerical_Analysis/c1s3p4.c
@@ -1,35 +1,29 @@
 //Computer Problem 1.3.4
 
-#include <stdio.h>
-#include <math.h>
-
-/* In terminal type:
-gcc -o filename filename.c -lm //This compiles the code
-./filename >> filename.out //This creates an output file
-./filename.out //This prints the output file in terminal
-*/
+# include <stdio.h>
+# include <math.h>
 
 main() {
-	double beta=pow(1.0-sqrt(3.0),-1);
-	double alpha=pow(2.0,-23);
+	double beta = pow(1.0 - sqrt(3.0), -1);
+	double alpha = pow(2.0, -23);
 	double x[101], y[101], z[101];
 	int n;
 
-	x[1]=1.0;
-	x[2]=1.0-sqrt(3.0);
+	x[1] = 1.0;
+	x[2] = 1.0 - sqrt(3.0);
 
-	for(n=3;n<=100;n++) {
-	      x[n]=2*(x[n-1]+x[n-2]);
+	for(n = 3; n <= 100; n++) {
+	      x[n] = 2 * (x[n-1] + x[n-2]);
 	}
 
-	for (n=1;n<=100;n++) {
-		y[n]=beta*pow(1.0-sqrt(3.0),n);
-		z[n]=alpha*pow(1.0+sqrt(3.0),n)+y[n];
+	for (n = 1; n <= 100; n++) {
+		y[n] = beta * pow(1.0 - sqrt(3.0), n);
+		z[n] = alpha * pow(1.0 + sqrt(3.0), n) + y[n];
 	}
 
-	printf("%3s\t %23s\t  %23s\t  %23s\t  %23s\n","n","x[n]","y[n]","z[n]","x[n]-z[n]"); // '\n' is for new line
+	printf("%3s \t %23s \t %23s\t %23s \t %23s\n", "n", "x[n]", "y[n]", "z[n]", "x[n]-z[n]");
 
-	for(n=1;n<=100;n++) {
-	   printf("%3d\t  %23.14e\t  %23.14e\t  %23.14e\t  %23.14e\n",n,x[n],y[n],z[n],x[n]-z[n]); // '\t' is for tab
+	for(n = 1; n <= 100; n++) {
+	   printf("%3d \t %23.14e \t %23.14e \t %23.14e\t %23.14e\n", n, x[n], y[n], z[n], x[n]-z[n]);
 	}
 } 
