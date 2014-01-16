b69fc58b41065e85ae46217af963e6137a327399
Divya Nair <dsnair@users.noreply.github.com>
2014-01-16 15:56:18 -0700

    find limit of division of two recurrence relations

--- UNR_Numerical_Analysis/c2s3p1.c
+++ UNR_Numerical_Analysis/c2s3p1.c
@@ -1,26 +1,26 @@
 //Computer Problem 2.3.1
 
-#include <stdio.h>
-#include <math.h>
+# include <stdio.h>
+# include <math.h>
 
 main() {
 	double A[21], B[21], C[21];
 	int n;
 
-	A[0]=pow(2.0,-23);
-	A[1]=1.0;
-	B[0]=1.0;
-	B[1]=1.0;
+	A[0] = pow(2.0, -23);
+	A[1] = 1.0;
+	B[0] = 1.0;
+	B[1] = 1.0;
 
-	for(n=2;n<20;n++) {
-			A[n]=n*A[n-1]+A[n-2];
-			B[n]=n*B[n-1]+B[n-2];
-			C[n]=A[n]/B[n];
+	for(n = 2; n < 20; n++) {
+			A[n] = n * A[n-1] + A[n-2];
+			B[n] = n * B[n-1] + B[n-2];
+			C[n] = A[n] / B[n];
 	}
 
-	printf("%3s\t %23s\t  %23s\t  %23s\n","n","A[n]","B[n]","A[n]/B[n]");
+	printf("%3s \t %23s \t %23s \t %23s \n", "n", "A[n]", "B[n]", "A[n]/B[n]");
 
-	for(n=0;n<20;n++) {
-	   printf("%3d\t  %23.14e\t  %23.14e\t  %23.14e\n", n, A[n], B[n], C[n]);
+	for(n = 0; n < 20; n++) {
+	   printf("%3d \t %23.14e \t %23.14e \t %23.14e \n", n, A[n], B[n], C[n]);
 	}
-} 
+}
