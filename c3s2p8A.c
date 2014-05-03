a18c319ed25274bf8734fe6cad579d55c0c51500
Divya Nair <dsnair@users.noreply.github.com>
2014-05-03 14:07:51 -0600

    complex numbers

--- c3s2p8A.c
+++ c3s2p8A.c
@@ -16,7 +16,7 @@ main() {
 	z = z0;
 	printf("%1s\t  %6s\t  %6s\n","#", "Real","Imaginary");
 	
-	for(j = 1; j <= N; j++) {
+	for (j = 1; j <= N; j++) {
 		f = z * z + 1;
 		df = 2 * z;
 		z = z0 - f / df;
@@ -25,6 +25,6 @@ main() {
 		Rez = creal(z);
 		Imz = cimag(z);
 		printf("%d\t %lf\t %lf i \n", j, Rez, Imz);
-		if(epsilon < delta) j = N + 1;
+		if (epsilon < delta) j = N + 1;
 	}	
 }	
