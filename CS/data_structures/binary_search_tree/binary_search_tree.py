594d4141fed2776dc6d365f184a2879830a6e414
Divya Nair <divnairs@gmail.com>
2019-07-23 12:34:15 -0700

    binary search tree: find max value

--- CS/data_structures/binary_search_tree/binary_search_tree.py
+++ CS/data_structures/binary_search_tree/binary_search_tree.py
@@ -4,8 +4,10 @@ class BinarySearchTree:
     self.left = None
     self.right = None
 
+
   def __str__(self):
     return "{} {} {}".format(self.value, self.left, self.right)
+
 
   def insert(self, value):
     # print(self.value, self.left, self.right)
@@ -22,9 +24,8 @@ class BinarySearchTree:
       else:
         self.left = BinarySearchTree(value)          
   
-  def contains(self, target):
-    # print(self.value, target, self.value == target)
 
+  def contains(self, target):
     if target < self.value:
       return self.left.contains(target) if self.left else False
     elif target > self.value:
@@ -32,19 +33,27 @@ class BinarySearchTree:
     else:
       return True
 
+
   def get_max(self):
-    pass
+    current = self
+    while current.right:
+      current = current.right
+    return current.value
 
   def for_each(self, cb):
     pass
+
 
 bst = BinarySearchTree(5)
 bst.insert(2)
 bst.insert(3)
 bst.insert(7)
 bst.insert(6)
+
 # print(bst)
 
 # print(bst.contains(2))
 # print(bst.contains(6))
 # print(bst.contains(8))
+
+print(bst.get_max())
