7f439d2de9f82c6843a2138272b799ee3827633f
Divya Nair <divnairs@gmail.com>
2019-08-06 14:37:48 -0700

    reorganize insert,remove,retrieve as class methods

--- CS/data_structures/hashtable/hashtable.py
+++ CS/data_structures/hashtable/hashtable.py
@@ -1,11 +1,12 @@
-# All storage values are initialized to None
-class BasicHashTable:
-    def __init__(self, size):
-        self.size = size
-        self.table = [None] * size
+# Linked List to handle collisions
+class LinkedPair:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
 
 
-# Implement the djb2 hash function
+# djb2 hash function
 def hash(string, max = 2 ** 32):
     h = 5381
     byte_array = string.encode('utf-8')
@@ -16,43 +17,47 @@ def hash(string, max = 2 ** 32):
     return h
 
 
-# If you are overwriting a value with a different key, print a warning
-def hash_table_insert(hash_table, key, value):
-    index = hash(key) % hash_table.size
-    if hash_table.table[index]:
-        print("You're overwriting the hash table key.")
-    else:
-        hash_table.table[index] = value
-    # print(hash_table.table[index])
+def resize(hash_table):
+    pass
 
 
-# If you try to remove a value that isn't there, print a warning
-def hash_table_remove(hash_table, key):
-    index = hash(key) % hash_table.size
-    if hash_table.table[index]:
-        hash_table.table[index] = None
-    else:
-        print("This value doesn't exist in the hash table.")
-    # print(hash_table.table[index])
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [None] * size
 
 
-# Should return None if the key is not found
-def hash_table_retrieve(hash_table, key):
-    index = hash(key) % hash_table.size
-    if index:
-        return hash_table.table[index]
-    else:
-        return None
+    # Handle collisions with Linked List
+    def insert(self, key, value):
+        index = hash(key) % self.size
+        if self.table[index]:
+            print("You're overwriting the hash table key.")
+        else:
+            self.table[index] = value
+        # print("Insert:", self.table[index])
 
 
-def Testing():
-    ht = BasicHashTable(16)
-    hash_table_insert(ht, "hello", "World!")
-    hash_table_remove(ht, "hello")
+    # If you try to remove a value that isn't there, print a warning
+    def remove(self, key):
+        index = hash(key) % self.size
+        if self.table[index]:
+            self.table[index] = None
+        else:
+            print("This value doesn't exist in the hash table.")
+        # print("Remove:", self.table[index])
 
-    if hash_table_retrieve(ht, "hello") is None:
-        print("Success!")
-    else:
-        print("Error!")
 
-Testing()
+    # Return None if the key is not found
+    def retrieve(self, key):
+        index = hash(key) % self.size
+        if index:
+            # print("Retrieve:", self.table[index])
+            return self.table[index]
+        else:
+            return None
+
+
+ht = HashTable(16)
+ht.insert("hello", "World!")
+ht.retrieve("hello")
+ht.remove("hello")
--- CS/data_structures/hashtable/hashtable_tests.py
+++ CS/data_structures/hashtable/hashtable_tests.py
@@ -1,38 +1,35 @@
 import unittest
 
-from hashtable import (BasicHashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve)
+from hashtable import HashTable
 
 
-class TestBasicHashTable(unittest.TestCase):
+class TestHashTable(unittest.TestCase):
 
     def test_hash_table_creation(self):
-        ht = BasicHashTable(8)
+        ht = HashTable(8)
 
         self.assertEqual(len(ht.table), 8)
         self.assertTrue(ht.table is not None)
 
     def test_hash_table_retrieval_of_initialized_value(self):
-        ht = BasicHashTable(8)
+        ht = HashTable(8)
 
-        return_value = hash_table_retrieve(ht, "key-0")
+        return_value = ht.retrieve("key-0")
         self.assertTrue(return_value is None)
 
     def test_hash_table_insertion_and_retrieval(self):
-        ht = BasicHashTable(8)
+        ht = HashTable(8)
 
-        hash_table_insert(ht, "key-0", "new-val-0")
-        return_value = hash_table_retrieve(ht, "key-0")
+        ht.insert("key-0", "new-val-0")
+        return_value = ht.retrieve("key-0")
         self.assertTrue(return_value == "new-val-0")
 
     def test_hash_table_removal(self):
-        ht = BasicHashTable(8)
+        ht = HashTable(8)
 
-        hash_table_insert(ht, "key-1", "new-val-1")
-        hash_table_remove(ht, "key-1")
-        return_value = hash_table_retrieve(ht, "key-1")
+        ht.insert("key-1", "new-val-1")
+        ht.remove("key-1")
+        return_value = ht.retrieve("key-1")
         self.assertTrue(return_value is None)
 
 
